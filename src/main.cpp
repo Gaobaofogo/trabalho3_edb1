3264e77e4eb653d8fcbd6d24525c844b44856488
Gabriel <gabrieldiniz54@gmail.com>
2020-12-07 23:10:39 -0300

    Calculando a distância depois de marcas todas as palavras erradas

--- src/main.cpp
+++ src/main.cpp
@@ -21,29 +21,30 @@ int main(int argc, char* argv[]) {
   std::map<std::string, MispelledWord> history_checked_words;
 
   for (size_t i = 0; i < test_data.size(); ++i) {
-    if (dict_data.second.count(test_data[i]) == 0) {
-      if (history_checked_words.count(test_data[i]) == 1) {
-        continue;
-      }
+    if (dict_data.second.count(test_data[i]) == 0
+        && history_checked_words.count(test_data[i]) == 0) {
 
       MispelledWord misspelled_word(test_data[i]);
 
-      for (size_t j = 0; j < dict_data.first.size(); ++j) {
-        int difference = dict_data.first[j].size() - test_data[i].size();
-
-        if (difference > 2 || difference < -2) {
-          continue;
-        }
-
-        misspelled_word.insert(dict_data.first[j]);
-
-        if (misspelled_word.suggestion.distance <= 2) {
-          break;
-        }
-      }
 
       misspelled_words.push_back(misspelled_word);
       history_checked_words[misspelled_word.word] = misspelled_word;
+    }
+  }
+
+  for (size_t i = 0; i < dict_data.first.size(); ++i) {
+    for (size_t j = 0; j < misspelled_words.size(); ++j) {
+      int difference = dict_data.first[i].size() - misspelled_words[j].word.size();
+
+      if (difference > 2 || difference < -2) {
+        continue;
+      }
+
+      if (misspelled_words[j].suggestion.distance <= 2) {
+        break;
+      }
+
+      misspelled_words[j].insert(dict_data.first[i]);
     }
   }
 
